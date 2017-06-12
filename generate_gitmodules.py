d6ca5495fdfa85cf9d8d3b2129f2c450d25f4c6a
Mickael MALLET <mickael.mallet@intel.com>
2017-06-12 11:23:40 +0200

    generate_gitmodules.py : Isolate the namespace ID from the commands

--- generate_gitmodules.py
+++ generate_gitmodules.py
@@ -4,6 +4,7 @@ import re
 import os
 import requests
 import json
+import sys
 
 #
 # Excepted format for the .gitmodules file:
@@ -11,15 +12,30 @@ import json
 #    path = meta-genivi-dev
 #    url = http://github.com/genivi/meta-genivi-dev.git
 
-# Ref to the github URL
-github_url = "tldlab23.tl.intel.com" #"github.forestscribe.intel.com"
+input_file = "list_project_path_names.txt"
+if len(sys.argv) > 1:
+    input_file = sys.argv[1]
+
+# Values for tldlab23
+token = "Ws6qyNwFo5Racz-jWvQC"
+github_url = "tldlab23.tl.intel.com"
+
+#
+# To get the namespaces ID, execute the following command for example:
+# curl --header "PRIVATE-TOKEN: eLzF-u6Pjmy-hFzUyZck" "https://gitlab.forestscribe.intel.com/api/v3/namespaces"
+#
+
+# Values for gitlab marathon
+token = "eLzF-u6Pjmy-hFzUyZck"
+token = "KkHWZXxeH9NeCcMcPd9E"
 github_url = "gitlab.forestscribe.intel.com"
 organization = "forestscribe-android"
-input_file = "list_project_path_names.txt"
+namespace_id = 11
+branch = "origin/HEAD"
 
-token_tldlab23 = "Ws6qyNwFo5Racz-jWvQC"
-token_gitlab_marathon = "eLzF-u6Pjmy-hFzUyZck"
-
+#organization = "forestscribe-yocto"
+#namespace_id = 19
+#branch =  "m/master"
 print "set -x"
 
 #os.system('repo list > {}'.format(input_file))
@@ -27,23 +43,27 @@ for line in open(input_file, "r"):
     line = line.strip()
     (path, name) = line.split(' : ')
     name = re.sub('/','_', name)
+    name = name.replace('.', '-')
     cmd = ""
 
     # To generate the script that will create all the repos
     if 0:
-        cmd += "curl --header \"PRIVATE-TOKEN: {}\" -X POST \"https://{}/api/v3/projects?name={}&visibility=public&namespace_id=11\"".format(token_gitlab_marathon, github_url, name)
+        cmd += "curl --header \"PRIVATE-TOKEN: {}\" -X POST \"https://{}/api/v3/projects?name={}&visibility=public&namespace_id={}\"".format(token, github_url, name, namespace_id)
         print cmd
 
     if 0:
-        print "git submodule add -f git@github.forestscribe.intel.com:forestscribe-android/{}.git {}".format(name, path)
+        # On gitlab . in project names is not supported
+        print "git submodule add -f git@{}:{}/{}.git {}".format(github_url, organization, name, path)
 
     # To push on github
     if 1:
         cmd = "cd {}\n".format(path)
         #cmd += "export GIT_SSL_NO_VERIFY=1"
-        cmd += "git fetch --unshallow\n"
-        cmd += "git branch -D master; git checkout origin/HEAD -b master\n"
-        cmd += "git push https://mickaelm:mickaelm@{}/{}/{} master\n".format(github_url, organization, name)
+        #cmd += "git fetch --unshallow\n"
+        #cmd += "git branch -D master; git checkout {} -b master\n".format(branch)
+        cmd += "git remote remove origin\n"
+        cmd += "git remote add origin git@{}:{}/{}\n".format(github_url, organization, name)
+        cmd += "git push origin HEAD:master\n"
         cmd += "cd -\n"
         print cmd
 
